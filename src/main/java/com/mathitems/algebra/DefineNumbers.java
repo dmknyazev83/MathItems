be3eec46c19ac46a73686be64d60c1042f819ea6
dmknyazev83 <dmknyazev83@gmail.com>
2019-10-28 00:21:03 +0300

    Add classes to generate pifagorent priples

--- src/main/java/com/mathitems/algebra/DefineNumbers.java
+++ src/main/java/com/mathitems/algebra/DefineNumbers.java
@@ -1,5 +1,10 @@
 package com.mathitems.algebra;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class DefineNumbers {
 	
 	public int[] defineIntegersAmongDoubles(double[] dArr){
@@ -15,6 +20,27 @@ public class DefineNumbers {
 			result[i] = (int)buff[i];
 		}
 		return result;
+	}
+	
+	public Integer[] defineIntegersAmongDoubles(Double[] dArr){
+		return Arrays.stream(dArr).filter(i -> i % 1 <= 0)
+		.map(i -> Integer.valueOf(i.intValue())).toArray(Integer[] :: new);
+	}
+	
+	public int[] defineIntegersPrimitivesAmongDoubles(Double[] dArr){
+		return Arrays.stream(dArr).filter(i -> i % 1 <= 0)
+		.mapToInt(i -> i.intValue()).toArray();
+	}
+	
+	public List<int[]> createPifagorTriaples(int limitRight){		
+	
+		List<int[]> res = IntStream.range(2, limitRight).boxed().flatMap(i -> {			
+			return IntStream.range(i, limitRight).filter(b-> Math.sqrt(i*i + b*b)% 1 <= 0).mapToObj(b -> 				
+				new int[]{i, b, (int)Math.sqrt(i*i + b*b)});		
+	 }).collect(Collectors.toList());
+		
+		return res;
+		
 	}
 
 }
--- src/test/java/com/mathitems/algebra/DefineNumbersTest.java
+++ src/test/java/com/mathitems/algebra/DefineNumbersTest.java
@@ -1,5 +1,7 @@
 package com.mathitems.algebra;
 
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,7 +16,7 @@ public class DefineNumbersTest {
 	}
 	
 	@Test
-	public void defineIntegersAmongDoublesTest(){
+	public void defineIntegersAmongDoublesTestPrimitives(){
 		double[] arrDouble1 = {3.4, 5.0, 2.1};
 		int[] expected1 = {5};
 		int[] actualArr1 = defineNumber.defineIntegersAmongDoubles(arrDouble1);
@@ -24,6 +26,33 @@ public class DefineNumbersTest {
 		int[] expected2 = {7, 6, 1};
 		int[] actualArr2 = defineNumber.defineIntegersAmongDoubles(arrDouble2);
 		Assert.assertArrayEquals(expected2, actualArr2);
+		
+	}
+	
+	@Test
+	public void defineIntegersAmongDoublesTestWrappers(){
+		Double[] dArr1 = {4.7D, 6.8D};
+		Integer[] intArr1 = defineNumber.defineIntegersAmongDoubles(dArr1);
+		Assert.assertTrue(intArr1.length == 0);
+		
+		Double[] dArr2 = {1.6D, 2.1D, 7.0D, 3.7D, 11D, 3.8D, 6.0D};
+		Integer[] intArr2 = defineNumber.defineIntegersAmongDoubles(dArr2);
+		Integer[] expected2 = {7, 11, 6};
+		Assert.assertArrayEquals(expected2, intArr2);
+	}
+	
+	@Test
+	public void defineIntegersPrimitivesTestAmongDoubles(){
+		Double[] dArr2 = {1.6D, 2.00D, 7.0D, 3.7D, 11D, 3.8D, 6.0D};
+		int[] intArr = defineNumber.defineIntegersPrimitivesAmongDoubles(dArr2);
+		int[] expected = {2, 7, 11, 6};
+		Assert.assertArrayEquals(expected, intArr);
+	}
+	
+	@Test
+	public void createPifagorTriaplesTest(){
+		List<int[]> actulTriples = defineNumber.createPifagorTriaples(10);
+		
 		
 	}
 
